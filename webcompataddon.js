a6fc4616d3a480cd01fbf209f038b266298c661f
soniasingla <soniasingla.1812@gmail.com>
2019-12-28 22:49:47 +0530

    Browser Action File added.

--- webcompataddon.js
+++ webcompataddon.js
@@ -1,3 +1,3 @@
-console.log("it works,otherwise kill patch!");
-//test krte abh chalo
-// chalo
+console.log("it works,otherwise kill patch!, patch is saved!");
+//https://github.com/mozilla/multi-account-containers/tree/master/src
+//https://staging.webcompat.com/issues/new
