5591fc67b36fd5447759feeb2a92d3890382e892
soniasingla <soniasingla.1812@gmail.com>
2019-12-28 23:22:01 +0530

    File Modified

--- webcompataddon.js
+++ webcompataddon.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 console.log("it works,otherwise kill patch!, patch is saved!");
 //https://github.com/mozilla/multi-account-containers/tree/master/src
 //https://staging.webcompat.com/issues/new
-=======
-console.log("it works,otherwise kill patch!");
->>>>>>> origin
