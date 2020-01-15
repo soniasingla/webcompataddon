34ae690e49c14d2bfcaa5ab5dac71306ae5a2a8d
soniasingla <soniasingla.1812@gmail.com>
2020-01-15 11:47:13 +0530

    Files modified with proper url

---
+++ js/checkurl.js
@@ -0,0 +1,13 @@
+import { URL } from "url";
+
+export default function isReportableURL(url) {
+  if (!URL) {
+    URL = window.URL;
+  }
+  try {
+    let protocol = new URL(url).protocol;
+    return ["http:", "https:"].includes(protocol);
+  } catch (error) {
+    return false;
+  }
+}
